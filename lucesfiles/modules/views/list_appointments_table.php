c89feb6744975bd96f30147f16ddc6efe1ccee1a
daniela <daniela.galue10@gmail.com>
2017-09-26 19:56:46 -0430

    New List Appointment

---
+++ lucesfiles/modules/views/list_appointments_table.php
@@ -0,0 +1,16 @@
+<h2>Lista de Citas del Paciente</h2>
+	<table>
+	<tr>
+		<th></th>
+		<th></th>
+		<th></th>
+		<th></th>
+
+	</tr>
+
+	<?php foreach ($appointments as  $a): ?>
+	<tr>
+		<td></td>	
+	</tr>	
+	<?php endforeach ?>
+	</table>
